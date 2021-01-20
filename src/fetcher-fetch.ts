b7de5373688a31596a3f0ae25b51a01d5147e976
Andrei Datcu <datcuandrei@gmail.com>
2021-01-20 18:13:12 +0200

    gitpkg

---
+++ src/fetcher-fetch.ts
@@ -0,0 +1,87 @@
+import { OperationDefinitionNode } from 'graphql';
+import { ReactQueryVisitor } from './visitor';
+import { FetcherRenderer } from './fetcher';
+
+export class FetchFetcher implements FetcherRenderer {
+  constructor(private visitor: ReactQueryVisitor) {}
+
+  generateFetcherImplementaion(): string {
+    return `
+function fetcher<TData, TVariables>(endpoint: string, requestInit: RequestInit, query: string, variables?: TVariables) {
+  return async (): Promise<TData> => {
+    const res = await fetch(endpoint, {
+      method: 'POST',
+      ...requestInit,
+      body: JSON.stringify({ query, variables }),
+    });
+
+    const json = await res.json();
+
+    if (json.errors) {
+      const { message } = json.errors[0];
+
+      throw new Error(message);
+    }
+
+    return json.data;
+  }
+}`;
+  }
+
+  generateQueryHook(
+    node: OperationDefinitionNode,
+    documentVariableName: string,
+    operationName: string,
+    operationResultType: string,
+    operationVariablesTypes: string,
+    hasRequiredVariables: boolean
+  ): string {
+    const variables = `variables${hasRequiredVariables ? '' : '?'}: ${operationVariablesTypes}`;
+    const hookConfig = this.visitor.queryMethodMap;
+    this.visitor.reactQueryIdentifiersInUse.add(hookConfig.query.hook);
+    this.visitor.reactQueryIdentifiersInUse.add(hookConfig.query.options);
+
+    const options = `options?: ${hookConfig.query.options}<${operationResultType}, TError, TData>`;
+
+    return `export const use${operationName} = <
+      TData = ${operationResultType},
+      TError = unknown
+    >(
+      dataSource: { endpoint: string, fetchParams?: RequestInit }, 
+      ${variables}, 
+      ${options}
+    ) => 
+    ${hookConfig.query.hook}<${operationResultType}, TError, TData>(
+      ['${node.name.value}', variables],
+      fetcher<${operationResultType}, ${operationVariablesTypes}>(dataSource.endpoint, dataSource.fetchParams || {}, ${documentVariableName}, variables),
+      options
+    );`;
+  }
+
+  generateMutationHook(
+    node: OperationDefinitionNode,
+    documentVariableName: string,
+    operationName: string,
+    operationResultType: string,
+    operationVariablesTypes: string
+  ): string {
+    const variables = `variables?: ${operationVariablesTypes}`;
+    const hookConfig = this.visitor.queryMethodMap;
+    this.visitor.reactQueryIdentifiersInUse.add(hookConfig.mutation.hook);
+    this.visitor.reactQueryIdentifiersInUse.add(hookConfig.mutation.options);
+
+    const options = `options?: ${hookConfig.mutation.options}<${operationResultType}, TError, ${operationVariablesTypes}, TContext>`;
+
+    return `export const use${operationName} = <
+      TError = unknown,
+      TContext = unknown
+    >(
+      dataSource: { endpoint: string, fetchParams?: RequestInit }, 
+      ${options}
+    ) => 
+    ${hookConfig.mutation.hook}<${operationResultType}, TError, ${operationVariablesTypes}, TContext>(
+      (${variables}) => fetcher<${operationResultType}, ${operationVariablesTypes}>(dataSource.endpoint, dataSource.fetchParams || {}, ${documentVariableName}, variables)(),
+      options
+    );`;
+  }
+}
---
+++ src/fetcher-graphql-request.ts
@@ -0,0 +1,75 @@
+import { OperationDefinitionNode } from 'graphql';
+import { ReactQueryVisitor } from './visitor';
+import { FetcherRenderer } from './fetcher';
+
+export class GraphQLRequestClientFetcher implements FetcherRenderer {
+  constructor(private visitor: ReactQueryVisitor) {}
+
+  generateFetcherImplementaion(): string {
+    return `
+function fetcher<TData, TVariables>(client: GraphQLClient, query: string, variables?: TVariables) {
+  return async (): Promise<TData> => client.request<TData, TVariables>(query, variables);
+}`;
+  }
+
+  generateQueryHook(
+    node: OperationDefinitionNode,
+    documentVariableName: string,
+    operationName: string,
+    operationResultType: string,
+    operationVariablesTypes: string,
+    hasRequiredVariables: boolean
+  ): string {
+    const variables = `variables${hasRequiredVariables ? '' : '?'}: ${operationVariablesTypes}`;
+    this.visitor.imports.add(`import { GraphQLClient } from 'graphql-request';`);
+
+    const hookConfig = this.visitor.queryMethodMap;
+    this.visitor.reactQueryIdentifiersInUse.add(hookConfig.query.hook);
+    this.visitor.reactQueryIdentifiersInUse.add(hookConfig.query.options);
+
+    const options = `options?: ${hookConfig.query.options}<${operationResultType}, TError, TData>`;
+
+    return `export const use${operationName} = <
+      TData = ${operationResultType},
+      TError = unknown
+    >(
+      client: GraphQLClient, 
+      ${variables}, 
+      ${options}
+    ) => 
+    ${hookConfig.query.hook}<${operationResultType}, TError, TData>(
+      ['${node.name.value}', variables],
+      fetcher<${operationResultType}, ${operationVariablesTypes}>(client, ${documentVariableName}, variables),
+      options
+    );`;
+  }
+
+  generateMutationHook(
+    node: OperationDefinitionNode,
+    documentVariableName: string,
+    operationName: string,
+    operationResultType: string,
+    operationVariablesTypes: string
+  ): string {
+    const variables = `variables?: ${operationVariablesTypes}`;
+    this.visitor.imports.add(`import { GraphQLClient } from 'graphql-request';`);
+
+    const hookConfig = this.visitor.queryMethodMap;
+    this.visitor.reactQueryIdentifiersInUse.add(hookConfig.mutation.hook);
+    this.visitor.reactQueryIdentifiersInUse.add(hookConfig.mutation.options);
+
+    const options = `options?: ${hookConfig.mutation.options}<${operationResultType}, TError, ${operationVariablesTypes}, TContext>`;
+
+    return `export const use${operationName} = <
+      TError = unknown,
+      TContext = unknown
+    >(
+      client: GraphQLClient, 
+      ${options}
+    ) => 
+    ${hookConfig.mutation.hook}<${operationResultType}, TError, ${operationVariablesTypes}, TContext>(
+      (${variables}) => fetcher<${operationResultType}, ${operationVariablesTypes}>(client, ${documentVariableName}, variables)(),
+      options
+    );`;
+  }
+}
